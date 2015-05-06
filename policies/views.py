ea16ecd3888c0b71f387a7b3d46add4a83cd5b8b
Ioram Schechtman Sette <iss@cin.ufpe.br>
2015-05-06 15:21:54 +0100

    Changed list and retrieve results according to the API. Added id to retrieve and list (since last commit)

--- policies/views.py
+++ policies/views.py
@@ -26,14 +26,50 @@ class PolicyViewSet(viewsets.ModelViewSet):
     def retrieve(self, request, pk=None):
         policy = models.Policy.objects.get(id=pk)
         serializer = PolicySerializer(policy)
-        resp = serializer.data
-        resp['content'] = openstack_parser.export_openstack_policy(pk)
+        resp = {}
+        resp['policy'] = serializer.data
+        resp['policy']['content'] = openstack_parser.export_openstack_policy(pk)
+        return Response(resp)
+
+    def list(self, request):
+        queryset = models.Policy.objects.all()
+        serializer = PolicySerializer(queryset, many=True)
+        resp = {}
+        resp['policies'] = serializer.data
         return Response(resp)
 
 class And_ruleViewSet(viewsets.ModelViewSet):
     queryset = models.And_rule.objects.all()
     serializer_class = And_ruleSerializer
 
+    def retrieve(self, request, pk=None):
+        and_rule = models.And_rule.objects.get(id=pk)
+        serializer = And_ruleSerializer(and_rule)
+        resp = {}
+        resp['and_rule'] = serializer.data
+        return Response(resp)
+
+    def list(self, request):
+        queryset = models.And_rule.objects.all()
+        serializer = And_ruleSerializer(queryset, many=True)
+        resp = {}
+        resp['and_rules'] = serializer.data
+        return Response(resp)
+
 class ConditionViewSet(viewsets.ModelViewSet):
     queryset = models.Condition.objects.all()
     serializer_class = ConditionSerializer
+
+    def retrieve(self, request, pk=None):
+        condition = models.Condition.objects.get(id=pk)
+        serializer = ConditionSerializer(condition)
+        resp = {}
+        resp['condition'] = serializer.data
+        return Response(resp)
+
+    def list(self, request):
+        queryset = models.Condition.objects.all()
+        serializer = ConditionSerializer(queryset, many=True)
+        resp = {}
+        resp['conditions'] = serializer.data
+        return Response(resp)
